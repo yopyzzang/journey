e9e328a1cd0f574658c6dc537cf74e971d9cd7d6
yopyzzang <yopyzzang@gmail.com>
2026-09-20 20:58:02 +0900

    feat: rolldownOption 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   },
   build: {
     manifest: true,
+    rolldownOptions: {
+      input: {
+        main: 'index.html',
+        projects: 'src/pages/projects/client.ts',
+        carrot: 'src/pages/carrot/client.ts',
+        career: 'src/pages/career/client.ts',
+      },
+    },
   },
 })
